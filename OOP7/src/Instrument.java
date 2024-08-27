16f7da467a1673261f2aef128746ce669ec67b3c
Süleyman Güzey <suleymanguzey590@gmail.com>
2024-08-27 21:29:34 +0300

    Implement Method Overriding and Late Binding in Instrument Hierarchy
    - Defined  class with a basic  method.
    - Added  class extending  to demonstrate inheritance.
    - Created  class extending , overriding  to provide specific behavior.
    - Demonstrated method overriding and late binding in  class, showing runtime polymorphism where the method implementation depends on the actual object type.
    
    This commit illustrates method overriding and late binding principles in object-oriented programming, enhancing the understanding of polymorphism.

---
+++ OOP7/src/Instrument.java
@@ -0,0 +1,9 @@
+public class Instrument {
+    /**
+     * Parameterless method.
+     * As it is in the parent class, it can be inherited by all its subclasses.
+     */
+    public void playAnInstrument(){
+        System.out.println("Sound...");
+    }
+}
