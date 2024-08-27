5ac7b07edd687d3e13a678984a22b124bb19ec18
Süleyman Güzey <suleymanguzey590@gmail.com>
2024-08-27 21:19:34 +0300

    Implement Inheritance in Vehicle Hierarchy
    - Added a  base class with protected properties and methods.
    - Created  class inheriting from  to extend functionality.
    - Implemented  class inheriting from  to further specialize the vehicle type.
    - Added  class extending  with specific attributes and methods.
    - Demonstrated inheritance through constructor chaining and method overriding in the  class.
    
    This commit leverages inheritance to enhance code reusability and project manageability, adhering to the principle of reusability and maintainability in object-oriented programming.

---
+++ OOP6/src/NonMotorizedLandVehicle.java
@@ -0,0 +1,12 @@
+public class NonMotorizedLandVehicle extends LandVehicle {
+    /**
+     * The "super()" method is added to the first line of the methods or
+     * constructors of derived classes, providing access to the methods and
+     * constructors of the superclasses.
+     * It is also found by default in the first line of the default constructors.
+     */
+    public NonMotorizedLandVehicle() {
+        super();
+        System.out.print(" Non-motorized land vehicle -");
+    }
+}
